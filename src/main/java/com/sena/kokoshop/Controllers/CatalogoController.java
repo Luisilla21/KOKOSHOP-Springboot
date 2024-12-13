a706713ef1f7f4c15adde786b08abd5da3b7ae38
VladK2706 <lunakevin200627@gmail.com>
2024-12-12 21:25:20 -0500

    imagenes de index y catalogo linkeado

---
+++ src/main/java/com/sena/kokoshop/Controllers/CatalogoController.java
@@ -0,0 +1,26 @@
+package com.sena.kokoshop.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sena.kokoshop.service.ProductoTallaService;
+
+@Controller
+public class CatalogoController {
+
+    @Autowired
+    private ProductoTallaService productoService;
+
+    @GetMapping("/catalogo")
+    public String listarProductosCatalogo(Model modelo){
+        modelo.addAttribute("productosTallas", productoService.listarTodosLosProductos());
+        return "catalogo"; // retorna al archivo productos
+        
+    }
+           
+    
+}
+

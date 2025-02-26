1122111f8448716aa85eb70148d63941b3bf4278
VladK2706 <lunakevin200627@gmail.com>
2025-02-25 20:31:37 -0500

    Intento Catalogo

--- src/main/java/com/sena/kokoshop/Controllers/CatalogoController.java
+++ src/main/java/com/sena/kokoshop/Controllers/CatalogoController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sena.kokoshop.interfaz.ProductoInterfaz;
-
 
 @Controller
 public class CatalogoController {
@@ -16,12 +14,10 @@ public class CatalogoController {
     private ProductoInterfaz interfaz;
 
     @GetMapping("/catalogo")
-    public String listarProductosCatalogo(Model modelo){
+    public String listarProductosCatalogo(Model modelo) {
         modelo.addAttribute("productos", interfaz.listarTodosLosProductos());
         return "catalogo"; // retorna al archivo productos
-        
-    }
-           
-    
-}
 
+    }
+
+}
